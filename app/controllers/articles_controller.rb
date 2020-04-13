eeae474b508b4d6eb75a5363bb17faddd020f5d9
furukawakimikazu <g.guquan@gmail.com>
2020-04-13 17:09:52 +0900

    create controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+  
+end
